4900f48e87158a96ee65fbe9d3581d21a66d8819
jhson <2297129184@qq.com>
2023-05-14 19:55:37 +0800

    Initial commit

--- src/main/java/com/example/canyon_gaming/controller/FilesController.java
+++ src/main/java/com/example/canyon_gaming/controller/FilesController.java
@@ -1,13 +1,20 @@
 package com.example.canyon_gaming.controller;
 
 
+import com.example.canyon_gaming.common.Result;
+import com.example.canyon_gaming.service.IFilesService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.IOException;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author sen
@@ -16,5 +23,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/files")
 public class FilesController {
+    @Resource
+    IFilesService filesService;
 
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws IOException {
+        return Result.success(filesService.upload(file));
+    }
 }
