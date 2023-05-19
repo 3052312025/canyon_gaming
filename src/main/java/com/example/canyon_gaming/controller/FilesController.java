deca9e2d0a7cf2f962ce7f9c77f44661355eb0cd
healme <121016021@qq.com>
2023-05-19 15:14:11 +0800

    zkl:第一次更新

--- src/main/java/com/example/canyon_gaming/controller/FilesController.java
+++ src/main/java/com/example/canyon_gaming/controller/FilesController.java
@@ -3,13 +3,12 @@ package com.example.canyon_gaming.controller;
 
 import com.example.canyon_gaming.common.Result;
 import com.example.canyon_gaming.service.IFilesService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -29,5 +28,9 @@ public class FilesController {
     @PostMapping("/upload")
     public Result upload(MultipartFile file) throws IOException {
         return Result.success(filesService.upload(file));
+    }
+    @GetMapping("/download/{uploadFileName}")
+    public void download(@PathVariable String uploadFileName, HttpServletResponse response) throws Exception{
+        filesService.download(uploadFileName,response);
     }
 }
--- src/main/java/com/example/canyon_gaming/entity/Files.java
+++ src/main/java/com/example/canyon_gaming/entity/Files.java
@@ -1,40 +1,35 @@
 package com.example.canyon_gaming.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 
 /**
- * <p>
  * 
- * </p>
- *
- * @author sen
- * @since 2023-05-14
+ * @TableName files
  */
+@TableName(value ="files")
 @Data
-@EqualsAndHashCode(callSuper = false)
-@Accessors(chain = true)
-@TableName("files")
-@AllArgsConstructor
-@NoArgsConstructor
 public class Files implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @TableId(value = "id", type = IdType.AUTO)
+    /**
+     * 
+     */
+    @TableId(type = IdType.AUTO)
     private Integer id;
 
-    private String filename;
-
+    /**
+     * 
+     */
     private String fileurl;
 
+    /**
+     * 
+     */
+    private String md5;
 
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
 }
