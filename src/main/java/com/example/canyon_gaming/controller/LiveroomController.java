735aca1867b35a6903569de29dd91506cb66a757
healme <121016021@qq.com>
2023-05-21 22:37:14 +0800

    zkl:第2次更新

--- src/main/java/com/example/canyon_gaming/controller/LiveroomController.java
+++ src/main/java/com/example/canyon_gaming/controller/LiveroomController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 /**
  * <p>
  *  前端控制器
@@ -22,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/liveroom")
 public class LiveroomController {
 
-    @Autowired
+    @Resource
     ILiveroomService liveroomService;
 
     //开播方法
