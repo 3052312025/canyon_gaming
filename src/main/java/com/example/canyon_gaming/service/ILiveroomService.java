e75e65f7935973b629e3e1ee2f79411bc7f8b9f0
黄松凯 <3052312025@qq.com>
2023-05-28 17:09:06 +0800

    skcommit8888

--- src/main/java/com/example/canyon_gaming/service/ILiveroomService.java
+++ src/main/java/com/example/canyon_gaming/service/ILiveroomService.java
@@ -26,7 +26,7 @@ public interface ILiveroomService extends IService<Liveroom> {
     String over(Integer id);
 
     //直播间点击方法
-    LiveroomDto touch(String roomId);
+    LiveroomDto touch(String roomId,Integer uid);
 
     //主页显示直播间
     List<LiveroomDto> show(Integer Page, Integer pageSize, String theme);
