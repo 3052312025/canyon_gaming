faef5440a1437474a3db6c986d5149ed11ea7cec
healme <121016021@qq.com>
2023-06-04 22:38:29 +0800

    zklzkl commi  6/4

--- src/main/java/com/example/canyon_gaming/service/impl/WorktimeServiceImpl.java
+++ src/main/java/com/example/canyon_gaming/service/impl/WorktimeServiceImpl.java
@@ -57,7 +57,6 @@ public class WorktimeServiceImpl extends ServiceImpl<WorktimeMapper, Worktime> i
         worktime.setStartTime(startTime1);
         worktime.setStopTime(stopTime2);
         worktimeMapper.insert(worktime);
-
         return "添加成功";
     }
 
